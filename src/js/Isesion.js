60266a419204a19431378777a7b280320db9cb13
Emanuel0az <emanuelabarcazuniga@gmail.com>
2024-05-09 15:45:55 -0600

    documentacion de las funciones

--- src/js/Isesion.js
+++ src/js/Isesion.js
@@ -2,14 +2,19 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const button = document.getElementById('button')
 
+// es un evento que al darle click busca valores en el LS y los compara con los que estan en los inputs para darle acceso 
 button.addEventListener('click', (e) => {
-    e.preventDefault()
+            e.preventDefault()
 
+    // obtiene los valores del local storage mediante el array
     usuarios = JSON.parse(localStorage.getItem("array")) || []
     console.log(usuarios);
+
+    // con el metodo find le pido que busque dichos valores el el LS y los compare 
     valores = usuarios.find(usu => usu.email == email.value && usu.password == password.value)
     console.log(valores)
 
+    // una condicion que siga que si son iguales los valores me de una alerta de bienvenido y si no otra alerta de error
     if (!valores) {
         swal("Error", "Contraseña ");
     } else {
@@ -25,7 +30,7 @@ button.addEventListener('click', (e) => {
     }
 })
 
-
+// hace que los espacios de los inputs queden en blanco
 document.getElementById('email').value = "";
 document.getElementById('password').value = "";
 
---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+button.addEventListener('click'){
+function guardarImagen() {
+    let archivo = document.getElementById('imageUploader').files[0];
+    let reader = new FileReader();
+    
+    reader.onload = function (e) {
+      localStorage.setItem('imagen', e.target.result);
+    };
+    
+    if (archivo) {
+      reader.readAsDataURL(archivo);
+    }
+  }
+}
+console.log(button)
+  
